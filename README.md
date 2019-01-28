0533deb35d02553199e6d802349580749051facd
Glitch (node-big-cache) <none>
2019-01-28 15:06:41 +0000

    🗃🙏 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # node-big-cache
+File based cache for large data. Data is stored in different files with different keys in a single folder.
+
+-----------------------------------------------------
+
+**Usage:**
+
+```
+var bigCache = require("node-big-cache");
+var cache = new bigCache(config);
+```
+
+config:
+```
+{
+  folder: "node-big-cache",
+  jsonFile: "node-big-cache-json"
+}
+```
+- folder: location of cache files. File name is created using a random key and extension is `.txt`. File key/name is always random and different than data key.
+- jsonFile: JSON database which stores all the matches of data key with file key. `.json` is added to the file automatically.
+
+------------------------------------------
+
+**Example (set):**
+
+```
+var bigCache = require("node-big-cache");
+var cache = new bigCache(config);
+cache.set("my_special_key", "This is some long data.");
+```
+
+Sets data for a key. Data is saved in utf-8 format and can be extremely long.
+
+**Example (get):**
+
+```
+var bigCache = require("node-big-cache");
+var cache = new bigCache(config);
+var data = cache.get("my_special_key");
+console.log(data);
+```
+
+Gets data for a key. Data is read in utf-8 format.
+
+**Example (list):**
+
+```
+var bigCache = require("node-big-cache");
+var cache = new bigCache(config);
+var list = cache.list();
+console.log(list);
+```
+
+Gets a list of all keys saved in database.
+
+**Example (remove):**
+
+```
+var bigCache = require("node-big-cache");
+var cache = new bigCache(config);
+cache.remove("my_special_key");
+```
+
+Removes any key and its data if exists.
+
+**Example (clear):**
+
+```
+var bigCache = require("node-big-cache");
+var cache = new bigCache(config);
+cache.clear();
+```
+
+Removes all the keys and their data.
---
+++ main.js
@@ -0,0 +1,63 @@
+module.exports = exports = function(config) {
+  var sanitize = require("node-sanitize-options");
+  var random = require("random-key");
+  var fileJson = require("node-file-json");
+  var fs = require("fs");
+  config = sanitize.options(config, {
+    folder: "node-big-cache",
+    jsonFile: "node-big-cache-json"
+  });
+  var json = new fileJson();
+  json.load(config.jsonFile + ".json");
+  if (fs.existsSync(config.folder) === false) {
+    fs.mkdirSync(config.folder);
+  }
+  var cache = {
+    list: function() {
+      return JSON.parse(JSON.stringify(json.data));
+    },
+    get: function(key) {
+      if (typeof key === "undefined" || !key) {
+        console.log("Key is not provided.");
+        return false;
+      }
+      if (typeof json.data[key] !== "undefined") {
+        var file = config.folder + "/" + json.data[key];
+        if (fs.existsSync(file) === true) {
+          return fs.readFileSync(file, "utf-8");
+        }
+      }
+    },
+    set: function(key, data) {
+      if (typeof key === "undefined" || !key) {
+        console.log("Key is not provided.");
+        return false;
+      }
+      if (typeof json.data[key] === "undefined" || !json.data[key]) {
+        json.data[key] = random.generate(32) + ".txt";
+        json.save();
+      }
+      if (typeof json.data[key] !== "undefined") {
+        var file = config.folder + "/" + json.data[key];
+        fs.writeFileSync(file, data, "utf-8");
+      }
+    },
+    remove: function(key) {
+      if (typeof json.data[key] !== "undefined") {
+        var file = config.folder + "/" + json.data[key];
+        if (fs.existsSync(file) === true) {
+          fs.unlinkSync(file);
+        }
+        delete json.data[key];
+        json.save();
+      }
+    },
+    clear: function() {
+      var list = cache.list();
+      for (var key in list) {
+        cache.remove(key);
+      }
+    }
+  };
+  return cache;
+};
